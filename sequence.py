badb821a3eb44d149a89da33b020cc2f8e306f19
MyraMidnight <dark_stardragon@hotmail.com>
2019-09-05 10:15:08 +0000

    commit before I redo things

--- sequence.py
+++ sequence.py
@@ -1,14 +1,14 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
 counter = 0
+collection = []
 
-
-while counter < n :
+while counter <= n :
   #placeholder for the printed number
   x = 0
-  x_range = []
-  #run through the range and add the numbers: 0 + 1 + 2 ... and then print the final number
-  for i in range(counter) :
-    x += range(counter)[-1]
-  print(x, end=", ")
+  if n > 3 :
+    #add the last three numbers from range to x
+    for i in range(counter, 0 , -1) :
+      print(i, end=", ") # + range(counter)[i-2] + range(counter)[i-3]
+  print()
   counter += 1
