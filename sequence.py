450f81b431b01600de9b847edda642cd6e6bb17e
MyraMidnight <dark_stardragon@hotmail.com>
2019-09-05 09:44:35 +0000

    getting the sequence to print correctly the sum of the last 3 numbers

--- sequence.py
+++ sequence.py
@@ -1,1 +1,13 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+counter = 0
+
+
+while counter < n :
+  #placeholder for the printed number
+  x = 1
+  #run through the range and add the numbers: 0 + 1 + 2 ... and then print the final number
+  for i in range(counter) :
+    x += i 
+  print(x, end=", ")
+  counter += 1
