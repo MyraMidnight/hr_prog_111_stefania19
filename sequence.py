c7b665ce0c6d8b8c10d9befe24860d7fbcb11de3
MyraMidnight <dark_stardragon@hotmail.com>
2019-09-05 11:01:27 +0000

    got it to work

--- sequence.py
+++ sequence.py
@@ -1,14 +1,20 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
 counter = 0
-collection = []
+collection = [1,2,3,6]
 
 while counter <= n :
   #placeholder for the printed number
   x = 0
   if n > 3 :
-    #add the last three numbers from range to x
-    for i in range(counter, 0 , -1) :
-      print(i, end=", ") # + range(counter)[i-2] + range(counter)[i-3]
-  print()
+    #if the number is larger than 3, it shall create the larger numbers
+    
+    for i in range(counter) :
+      x = collection[-1] + collection[-2] + collection[-3]
+      #push the new sum to the collection
+      collection.append(x)
+  new_collection = collection[0:n]
   counter += 1
+
+for i in range(n) :
+  print(collection[i], end=", ")
